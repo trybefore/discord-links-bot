f381a5b58b9512185739a49def3a163c7c6e3a74
Daniel Boman <daniel.boman@pm.me>
2022-10-21 23:50:55 +0200

    finished?

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/trybefore/fxdiscordbot
+
+go 1.18
+
+require (
+	github.com/diamondburned/arikawa/v3 v3.1.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20211001092434-39dca1131b70 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/diamondburned/arikawa/v3/api"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+	"github.com/diamondburned/arikawa/v3/session"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	var token = os.Getenv("BOT_TOKEN")
+
+	if token == "" {
+		log.Fatalln("$BOT_TOKEN missing")
+	}
+
+	s := session.New("Bot " + token)
+
+	queue := make(chan message)
+
+	s.AddHandler(func(c *gateway.MessageCreateEvent) {
+		if strings.HasPrefix(c.Message.Content, "https://twitter.com") {
+			queue <- message{
+				content: c.Message,
+				author:  c.Author,
+			}
+		}
+	})
+
+	s.AddIntents(gateway.IntentDirectMessages)
+	s.AddIntents(gateway.IntentGuildMessages)
+
+	if err := s.Open(context.Background()); err != nil {
+		log.Fatalln("Failed to connect:", err)
+	}
+
+	defer s.Close()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func(s *session.Session) {
+		for msg := range queue {
+			go replaceMessage(s, msg)
+		}
+	}(s)
+
+	<-c
+	close(queue)
+}
+
+func replaceMessage(s *session.Session, m message) {
+
+	newMessage := fmt.Sprintf(`from: %s
+	%s
+	`, m.author.Mention(), strings.Replace(m.content.Content, "https://twitter.com", "https://fxtwitter.com", 1))
+
+	_, err := s.SendMessage(m.content.ChannelID, newMessage)
+	if err != nil {
+		log.Println("error sending message:", err)
+	}
+	if err = s.DeleteMessage(m.content.ChannelID, m.content.ID, api.AuditLogReason("fxtwitterbot")); err != nil {
+		log.Println("error deleting message:", err)
+	}
+}
+
+type message struct {
+	content discord.Message
+	author  discord.User
+}
