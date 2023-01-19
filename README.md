6c690362a36fafadea48eb6a21914e499936d363
Daniel Boman <daniel.boman@pm.me>
2023-01-19 21:45:52 +0100

    replace discord media links with discord cdn links (#13)
    * replace media.discordapp.net links with cdn.discordapp.com

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A discord bot for replacing/trimming common links with more appropriate versions
 
 ## Supported links
-- twitter links are replaced with fxtwitter links
+- twitter links are replaced with fxtwitter links (bot hides original message's embed)
+- media.discordapp.net links are replaced with cdn.discordapp.com (bot does nothing to the original message, for now)
 
 
--- replacer_test.go
+++ replacer_test.go
@@ -5,38 +5,60 @@ import (
 	"testing"
 )
 
+func TestDiscordRegex(t *testing.T) {
+	replacer := &genericReplacer{
+		regex:       regexp.MustCompile(`https?:\/\/media\.discordapp\.net/attachments/(\d+)/(\d+)/(.*)`),
+		replacement: "https://cdn.discordapp.com/attachments/$1/$2/$3",
+	}
+	tests := []struct {
+		want string
+		have string
+	}{
+		{
+			have: "https://media.discordapp.net/attachments/483348725704556557/1065345579762335915/v12044gd0000cf3g5rrc77u1ikgnhp8g.mp4",
+			want: "https://cdn.discordapp.com/attachments/483348725704556557/1065345579762335915/v12044gd0000cf3g5rrc77u1ikgnhp8g.mp4",
+		},
+
+		//https://media.discordapp.net/attachments/483348725704556557/1065345579762335915/v12044gd0000cf3g5rrc77u1ikgnhp8g.mp4
+		//https://cdn.discordapp.com/attachments/735399993485033472/1065376405921222686/v12044gd0000cf3g5rrc77u1ikgnhp8g.mp4
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.have, func(t *testing.T) {
+			if got := replacer.Replace(tt.have); got != tt.want {
+				t.Fatalf("%s != %s", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestTwitterRegex(t *testing.T) {
 	replacer := &genericReplacer{regex: regexp.MustCompile(`https?:\/\/(?P<tld>twitter)\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)`), replacement: "https://fxtwitter.com/$2/status/$4"}
 
 	tests := []struct {
-		replacer *genericReplacer
-		want     string
-		have     string
+		want string
+		have string
 	}{
 		{
-			replacer: replacer,
-			have:     "https://twitter.com/blablabla/status/12345678910",
-			want:     "https://fxtwitter.com/blablabla/status/12345678910",
+			have: "https://twitter.com/blablabla/status/12345678910",
+			want: "https://fxtwitter.com/blablabla/status/12345678910",
 		},
 		{
-			replacer: replacer,
-			have:     "https://twitter.com/blablabla/statuses/12345678910",
-			want:     "https://fxtwitter.com/blablabla/status/12345678910",
+			have: "https://twitter.com/blablabla/statuses/12345678910",
+			want: "https://fxtwitter.com/blablabla/status/12345678910",
 		},
 		{
-			replacer: replacer,
-			have:     "https://twitter.com/blablabla/statuses/12345678910?cheese=doodles",
-			want:     "https://fxtwitter.com/blablabla/status/12345678910",
+			have: "https://twitter.com/blablabla/statuses/12345678910?cheese=doodles",
+			want: "https://fxtwitter.com/blablabla/status/12345678910",
 		},
 		{
-			replacer: replacer,
-			have:     "http://twitter.com/blablabla/statuses/12345678910",
-			want:     "https://fxtwitter.com/blablabla/status/12345678910",
+			have: "http://twitter.com/blablabla/statuses/12345678910",
+			want: "https://fxtwitter.com/blablabla/status/12345678910",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.have, func(t *testing.T) {
-			if got := tt.replacer.Replace(tt.have); got != tt.want {
+			if got := replacer.Replace(tt.have); got != tt.want {
 				t.Fatalf("%s != %s", got, tt.want)
 			}
 		})
